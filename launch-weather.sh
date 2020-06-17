afc60ce3fc9608ca5d7474203614c7b4253c0899
chrisley75 <christopher.ley@gmail.com>
2020-06-17 20:04:54 +0200

    Fix bug

--- launch-weather.sh
+++ launch-weather.sh
@@ -11,12 +11,12 @@ if [ -z "${ps}" ]; then
 	echo "chrisley's weather app is not running"
 	echo "starting app..."
 	# Launch chrisley's weather app
-	nohup node ${path}/bin/www >> ${path}/weather.log &
+	node ${path}/bin/www >> ${path}/weather.log
 else
 	echo "chrisley's app is running with PID:" ${ps}
 	echo "restarting app..."
 	kill -15 ${ps}
 	sleep 1
 	# Launch chrisley's weather app
-	nohup node ${path}/bin/www >> ${path}/weather.log &
+	node ${path}/bin/www >> ${path}/weather.log
 fi
