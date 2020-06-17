e036542f0ac5c6783b53c7049d888fa28f87686b
chrisley75 <christopher.ley@gmail.com>
2020-06-17 18:36:02 +0200

    Enhance launch script to control and kill previous process if exists

--- launch-weather.sh
+++ launch-weather.sh
@@ -3,4 +3,10 @@
 
 path=/var/www/weather
 
+# Kill previous process
+ps=`pgrep node`
+echo kill node process: ${ps}
+kill -15 ${ps}
+
+# Launch chrisley's weather app
 nohup node ${path}/bin/www >> ${path}/weather.log &
